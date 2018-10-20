861fa4b6d46873562aa1fdb3eca3d9e73d58c0e5
GrumpyEwok <evanfilkins@gmail.com>
2018-10-19 17:07:47 -0700

    Properly displayed the alphabetical characters inside Letters.

--- angular-rebuild/src/app/content-body/content-body.component.ts
+++ angular-rebuild/src/app/content-body/content-body.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-body.component.css']
 })
 export class ContentBodyComponent implements OnInit {
-  public letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  public letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
   constructor() { }
 
